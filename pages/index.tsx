30263bd268dc2df71690df43f643e7ac3e16b133
Lachlan Sleight <lachlansleight@gmail.com>
2022-01-18 23:53:49 +1100

    feat: Make homepage nicer + show error messages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,46 +1,72 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import { applyCypher } from "lib/encoding";
 import Layout from "../components/layout/Layout";
+import wordList from "../lib/wordList";
 
 export const Home = (): JSX.Element => {
     const router = useRouter();
     const [target, setTarget] = useState("");
     const [guesses, setGuesses] = useState("");
+    const [error, setError] = useState("");
+    const [isValid, setIsValid] = useState(false);
 
-    const isValid = (target: string, guesses: string) => {
-        if (target.length !== 5) return false;
-        const guessArray = guesses.split("\n");
-        if (guessArray.length === 0) return false;
-        for (let i = 0; i < guessArray.length; i++) {
-            if (guessArray[i].length !== 5) return false;
-            if (!/[a-z][a-z][a-z][a-z][a-z]/i.test(guessArray[i])) return false;
+    useEffect(() => {
+        if(!target && !guesses) {
+            setError("");
+            setIsValid(false);
+            return;
         }
-        return true;
-    };
+
+        
+        if(!wordList.includes(target)) {
+            setError("Solution '" + target + "' isn't in the word list");
+            setIsValid(false);
+            return;
+        }
+        const guessArray = guesses.split("\n").filter(guess => guess.length > 0);
+        if (guessArray.length === 0) {
+            setError("Need at least one guess");
+            setIsValid(false);
+            return;
+        }
+        for (let i = 0; i < guessArray.length; i++) {
+            if(!wordList.includes(guessArray[i])) {
+                setError("Word list doesn't contain '" + guessArray[i] + "'");
+                setIsValid(false);
+                return;
+            }
+        }
+        setError("");
+        setIsValid(true);
+    }, [target, guesses]);
 
     return (
         <Layout>
             <div className="flex flex-col gap-4">
-                <h1 className="text-2xl mb-4">Input your game data</h1>
+                
+                <p className="text-xl">Wordle Report will show you how quickly you narrowed down the puzzle&apos;s possible solutions.</p>
+                <p className="text-xl">Find out whether you played well, or got lucky!</p>
+                <hr/>
+
                 <label>Today&apos;s Solution</label>
                 <input
-                    className="bg-neutral-800 text-white rounded px-2 py-1"
+                    className="bg-neutral-800 text-white rounded px-2 py-1 text-2xl"
                     type="text"
                     value={target}
                     onChange={e => setTarget(e.target.value.toLowerCase())}
                 />
                 <label>Your Guesses (in order, one per line)</label>
                 <textarea
-                    className="bg-neutral-800 text-white rounded px-2 py-1 h-36"
+                    className="bg-neutral-800 text-white rounded px-2 py-1 h-48 text-2xl"
                     value={guesses}
                     onChange={e => setGuesses(e.target.value.toLowerCase())}
                 />
                 <button
                     className={`text-xl py-2 rounded ${
-                        !isValid(target, guesses) ? "bg-red-700" : "bg-green-700 cursor-pointer"
+                        isValid ? "bg-green-700 cursor-pointer" : "bg-red-700"
                     }`}
-                    disabled={!isValid(target, guesses)}
+                    disabled={!isValid}
                     onClick={() =>
                         router.push(
                             `/report?target=${applyCypher(target, 10)}&guesses=${guesses
@@ -52,6 +78,7 @@ export const Home = (): JSX.Element => {
                 >
                     Show Report
                 </button>
+                {error && <p className="text-2xl text-red-400 text-center">{error}</p>}
             </div>
         </Layout>
     );
